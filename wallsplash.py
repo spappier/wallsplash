b6840557152ca24ed9a63adb806620246364841a
Santiago Pappier <spappier@gmail.com>
2016-02-03 01:01:57 -0300

    pass a category, user and/or query when switching + docopt for argument parsing

--- wallsplash.py
+++ wallsplash.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python2
+'''
+Wallsplash
+
+Usage:
+  wallsplash ( [--category=<category>] | [--user=<user>] ) [--query=<query>]
+  wallsplash --version
+  wallsplash -h | --help
+
+Options:
+  --category=<category>  Category to pull from.
+  --user=<user>          User to pull from.
+  --query=<query>        Comma-separated search terms.
+  --version              Show version.
+  -h --help              Show this screen.
+'''
 
 import subprocess
 import urlparse
@@ -6,10 +21,12 @@ import os
 import re
 
 import requests
+import docopt
 
-BASE_URL = 'https://source.unsplash.com/random/1280x800'
+BASE_URL = 'https://source.unsplash.com'
 WALLPAPER_SIZE = '1280x800'
 IMAGES_FOLDER = '/tmp'
+CATEGORIES = ('buildings', 'food', 'nature', 'people', 'technology', 'objects')
 
 
 def download_wallpaper(url):
@@ -23,8 +40,10 @@ def download_wallpaper(url):
     return image_path
 
 
-def get_wallpaper_url():
-    r = requests.get(BASE_URL, allow_redirects=False)
+def get_wallpaper_url(resource, query=None):
+    params = { '': query } if query else None
+    r = requests.get('{}/{}/{}'.format(BASE_URL, resource, WALLPAPER_SIZE),
+                     params=params, allow_redirects=False)
     r.raise_for_status()
     if not r.is_redirect:
         print('not a redirect')
@@ -37,10 +56,25 @@ def set_wallpaper(fn):
                      'picture-uri', fn])
 
 
-def main():
-    url = get_wallpaper_url()
+def switch_wallpaper(category=None, user=None, query=None):
+    if category:
+        if category not in CATEGORIES:
+            print('category should be one of: {}'.format(', '.join(CATEGORIES)))
+            raise SystemExit(1)
+        resource = 'category/{}'.format(category)
+    elif user:
+        resource = 'user/{}'.format(user)
+    else:
+        resource = 'random'
+    url = get_wallpaper_url(resource, query=query)
     fn = download_wallpaper(url)
     set_wallpaper(fn)
+
+
+def main():
+    args = docopt.docopt(__doc__, version='0.0.2')
+    switch_wallpaper(args.get('--category'), args.get('--user'),
+                     args.get('--query'))
 
 
 if __name__ == '__main__':
